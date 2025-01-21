2ce28c21c1dc884cf5c430d23d0398d87839a0f1
Oleg088 <94531822+Oleg44354354215@users.noreply.github.com>
2025-01-21 15:17:06 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Profile from "./components/Profile/Profile";
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+
+import userData from "./data/userData.json";
+import friends from "./data/friends.json"
+import transaction from  "./data/transaction.json"
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transaction} />
+    </>
+  );
+}
+export default App;
